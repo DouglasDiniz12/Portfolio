ef88ddc96fdbe2955ee04aa84f39a6a8ca3dc786
DouglasDiniz12 <73031825+DouglasDiniz12@users.noreply.github.com>
2024-07-19 16:32:25 -0300

    diminuir title

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,10 +3,10 @@
 const AboutContent = () => {
   return (
   <section className="about-container">
-    <h2>Olá! Atuo como Desenvolvedor Front-End</h2>
-    <h2>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
+    <h3>Olá! Atuo como Desenvolvedor Front-End</h3>
+    <h4>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
 criando sites para clientes  utilizando JavaScript, HTML, Sass, CSS, TypeScripte, React js outras 
-demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</h2>
+demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</h4>
     <p>
 
     </p>
