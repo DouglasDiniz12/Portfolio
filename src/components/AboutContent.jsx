7a2e1ca4330bfde4eef6248c2887f257bca68a6f
DouglasDiniz12 <73031825+DouglasDiniz12@users.noreply.github.com>
2024-07-19 16:41:56 -0300

    ppp

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,10 +3,10 @@
 const AboutContent = () => {
   return (
   <section className="about-container">
-    <h3>Olá! Atuo como Desenvolvedor Front-End</h3>
-    <h4>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
+    <p> Olá! Atuo como Desenvolvedor Front-End</p>
+    <p>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
 criando sites para clientes  utilizando JavaScript, HTML, Sass, CSS, TypeScripte, React js outras 
-demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</h4>
+demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</p>
     <p>
 
     </p>
