9e11f35b822a044425688d209e97ee43f50e4c86
DouglasDiniz12 <73031825+DouglasDiniz12@users.noreply.github.com>
2024-07-19 16:15:07 -0300

    mudanca de titulo

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,7 +3,7 @@
 const AboutContent = () => {
   return (
   <section className="about-container">
-    <h1>Olá! Atuo como Desenvolvedor Front-End</h1>
+    <h2>Olá! Atuo como Desenvolvedor Front-End</h2>
     <h2>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
 criando sites para clientes  utilizando JavaScript, HTML, Sass, CSS, TypeScripte, React js outras 
 demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</h2>
