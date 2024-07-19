82249d713ca36cf71b712d0a9859c4d26671e32c
DouglasDiniz12 <73031825+DouglasDiniz12@users.noreply.github.com>
2024-07-19 16:09:34 -0300

    mudanca na intro

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,8 +3,11 @@
 const AboutContent = () => {
   return (
   <section className="about-container">
-    <h2>sobre</h2>
-    <p>Recentemente fiz uma transição de carreira para desenvolvimento Frontend e tenho trabalhado com freelancer nos últimos 4 meses. Minha especialidade inclui React,CSS,Sass,javascript
+    <h1>Olá! Atuo como Desenvolvedor Front-End</h1>
+    <h2>Recentemente atuo como desenvolvedor Desenvolvimento Front-End,
+criando sites para clientes  utilizando JavaScript, HTML, Sass, CSS, TypeScripte, React js outras 
+demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</h2>
+    <p>
 
     </p>
   
