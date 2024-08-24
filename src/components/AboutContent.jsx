b67f908ae582f0e21398e79c9c068c7c081d1849
DouglasDiniz12 <73031825+DouglasDiniz12@users.noreply.github.com>
2024-08-24 11:17:52 -0300

    adicao de novos aquivos

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,10 +3,12 @@
 const AboutContent = () => {
   return (
   <section className="about-container">
+
+  <h2>sobre</h2>
+
     <p>Recentemente atuo como desenvolvedor  Front-End
 criando sites para clientes  utilizando JavaScript, HTML, Sass, CSS, TypeScripte, React js outras 
-demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue</p>
-    <p>
+demais ferramentas. Atualmente estou estudando novas tecnologias, como  Angular js e Vue
 
     </p>
   
